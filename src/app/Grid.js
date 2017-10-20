6797a7ab3ee552f145998a47bf3c3b19a80b05d4
Daniele Damiani <danieledamianiuk@gmail.com>
2017-10-20 06:51:52 +0100

    refactor Grid to class and add specs

--- src/app/Grid.js
+++ src/app/Grid.js
@@ -4,26 +4,28 @@
 // Any live cell with more than three live neighbours dies, as if by overpopulation.
 // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
-function Grid(rows, columns) {
-  let _rows = rows;
-  let _columns = columns;
-  let _grids = [];
-  let _iterations;
-
-  function _isElementOutsideGrid(elementRow, elementColumn) {
-    return elementRow < 0 || elementRow > _rows - 1 || elementColumn < 0 || elementColumn > _columns - 1;
+class Grid {
+  constructor(rows, columns) {
+    this._rows = rows;
+    this._columns = columns;
+    this._grids = [];
+    this._iterations;
   }
 
-  function _getElement(elementRow, elementColumn) {
-    return _isElementOutsideGrid(elementRow, elementColumn) ? null : _getGrid()[elementRow][elementColumn];
+  isElementOutsideGrid(elementRow, elementColumn) {
+    return elementRow < 0 || elementRow > this._rows - 1 || elementColumn < 0 || elementColumn > this._columns - 1;
   }
 
-  function _getAliveNeighbours(neighbours) {
+  getElement(elementRow, elementColumn) {
+    return this.isElementOutsideGrid(elementRow, elementColumn) ? null : this.getGrid()[elementRow][elementColumn];
+  }
+
+  getAliveNeighbours(neighbours) {
     return neighbours.filter(function(element) { return element; }).length;
   }
 
-  function _applyRules(elementRow, elementColumn, aliveNeighbours) {
-    let alive = _getElement(elementRow, elementColumn);
+  applyRules(elementRow, elementColumn, aliveNeighbours) {
+    let alive = this.getElement(elementRow, elementColumn);
     if( (aliveNeighbours < 2 || aliveNeighbours > 3) && alive) {
       return !alive;
     }
@@ -34,64 +36,57 @@ function Grid(rows, columns) {
     return alive;
   }
 
-  function _init() {
+  init() {
     let grid = [];
-    _iterations = 0;
-    for(let rowsIterator = 0; rowsIterator < _rows; rowsIterator++) {
+    this._iterations = 0;
+    for(let rowsIterator = 0; rowsIterator < this._rows; rowsIterator++) {
       let row = [];
-      for(let columnsIterator = 0; columnsIterator < _columns; columnsIterator++) {
+      for(let columnsIterator = 0; columnsIterator < this._columns; columnsIterator++) {
         row[columnsIterator] = Math.random() < 0.5 ? true : false;
       }
       grid.push(row);
     }
-    _grids.push(grid);
+    this._grids.push(grid);
   }
 
-  function _getGrid() {
-    return _grids[_grids.length - 1];
+   getGrid() {
+    return this._grids[this._grids.length - 1];
   }
 
-  function _setGrid(grid) {
-    _grids.push(grid);
+   setGrid(grid) {
+    this._grids.push(grid);
   }
 
-  function _getElementNeighbours(elementRow, elementColumn) {
+   getElementNeighbours(elementRow, elementColumn) {
     let neighbours = [];
     for(let rowsIterator = elementRow - 1; rowsIterator < elementRow + 2; rowsIterator++) {
       for(let columnsIterator = elementColumn - 1; columnsIterator < elementColumn + 2; columnsIterator++) {
         if(!(rowsIterator == elementRow && columnsIterator == elementColumn)) {
-          _isElementOutsideGrid(rowsIterator, columnsIterator) ? null : neighbours.push(_getElement(rowsIterator, columnsIterator));
+          this.isElementOutsideGrid(rowsIterator, columnsIterator) ? null : neighbours.push(this.getElement(rowsIterator, columnsIterator));
         }
       }
     }
     return neighbours;
   }
 
-  function _iterate() {
-    _iterations++;
+  iterate() {
+   this._iterations++;
     let nextGrid = [];
-    for(let rowsIterator = 0; rowsIterator < _rows; rowsIterator++) {
+    for(let rowsIterator = 0; rowsIterator < this._rows; rowsIterator++) {
       let row = [];
-      for(let columnsIterator = 0; columnsIterator < _columns; columnsIterator++) {
-        let neighbours = _getElementNeighbours(rowsIterator, columnsIterator);
-        let aliveNeighbours = _getAliveNeighbours(neighbours);
-        row[columnsIterator] = _applyRules.call(this, rowsIterator, columnsIterator, aliveNeighbours);
+      for(let columnsIterator = 0; columnsIterator < this._columns; columnsIterator++) {
+        let neighbours = this.getElementNeighbours(rowsIterator, columnsIterator);
+        let aliveNeighbours = this.getAliveNeighbours(neighbours);
+        row[columnsIterator] = this.applyRules.call(this, rowsIterator, columnsIterator, aliveNeighbours);
       }
       nextGrid.push(row);
     }
-    _setGrid(nextGrid);
+    this.setGrid(nextGrid);
   }
 
-  function _getIterations() {
-    return _iterations;
-  }
-
-  return {
-    init: _init,
-    getGrid: _getGrid,
-    iterate: _iterate,
-    getIterations: _getIterations
+   getIterations() {
+    return this._iterations;
   }
 }
 
-module.exports = Grid;
+export default Grid;
