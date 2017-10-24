d60b779116c0b270dd2d32a9aed6f1f83aedc11c
Daniele Damiani <danieledamianiuk@gmail.com>
2017-10-24 21:41:47 +0100

    fix asset path

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -17,7 +17,7 @@ export default {
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: './',
     filename: '[name].[chunkhash].js'
   },
   plugins: [
