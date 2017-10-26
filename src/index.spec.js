66d5ade9af2e1e1e7a0c1ba980e08ece0a9cbd95
Daniele Damiani <danieledamianiuk@gmail.com>
2017-10-26 14:51:35 +0100

    clean index.spec

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,6 +1,4 @@
 import  { expect } from 'chai';
-// import jsdom from 'jsdom/lib/old-api.js';
-// import fs from 'fs';
 
 describe('My first test', () => {
   it('should pass', () => {
@@ -8,15 +6,3 @@ describe('My first test', () => {
   });
 });
 
-// describe('index.html', () => {
-//   it('should have h1 that says Users', (done) => {
-//     const index = fs.readFileSync('./src/index.html', 'utf-8');
-//     jsdom.env(index, (err, window) => {
-//       const h1 = window.document.getElementsByTagName('h1')[0];
-//       const expected = 'Users';
-//       expect(h1.innerHTML).to.equal(expected);
-//       done();
-//       window.close();
-//     });
-//   });
-// });
